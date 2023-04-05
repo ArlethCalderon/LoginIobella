4ee5c6fbaa3442f4ef1480483f5c0b7a61a5ac17
Arleth Calderon <arletvalle78@gmail.com>
2023-04-05 08:27:17 -0700

    login y boton feo

---
+++ Index.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Login_iobella
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["usuariologueado"] != null)
+            {
+                string usuariologueado = Session["usuariologueado"].ToString();
+                lblBienvenida.Text = "Bienvenida/o " +  usuariologueado;
+            }
+            else
+            {
+                Response.Redirect("login.aspx");
+            }
+        }
+        protected void BtnCerrar_Click(object sender, EventArgs e)
+        {
+            Session.Remove("usuariologueado");
+            Response.Redirect("login.aspx");
+        }
+    }
+
+}
---
+++ login.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Login_iobella
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string patron = "1234";
+        protected void BtnIngresar_Click (object sender, EventArgs e )
+        {
+            string conectar = ConfigurationManager.ConnectionStrings["conexion"].ConnectionString;
+            SqlConnection sqlconectar = new SqlConnection(conectar);
+            SqlCommand cmd = new SqlCommand("SP_ValidarUsuario", sqlconectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd.Connection.Open();
+            cmd.Parameters.Add("@Usuario", SqlDbType.VarChar, 50).Value = tbUsuario.Text;
+            cmd.Parameters.Add("@Clave", SqlDbType.VarChar, 50).Value = tbPassword.Text;
+            cmd.Parameters.Add("@Patron", SqlDbType.VarChar, 50).Value = patron;
+            SqlDataReader dr = cmd.ExecuteReader();
+            if(dr.Read())
+            {
+                //Agregando una sesion de usuario
+                Session["usuariologueado"] = tbUsuario.Text;
+                Response.Redirect("Index.aspx");
+            }
+            else
+            {
+                lblError.Text = "Error de Usuario o Contraseña";
+            }
+            cmd.Connection.Close();
+
+        }
+    }
+}
